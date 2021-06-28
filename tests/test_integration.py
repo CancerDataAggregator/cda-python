ebff17a4597dceb647c52f53982cdfa98de25e56
fkaufman-asym <fkaufman@asymmetrik.com>
2021-06-28 16:58:44 -0400

    add really basic integration test for unique_terms

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,12 @@
 from cdapython import columns
+from cdapython import unique_terms
 
 
 def test_basic_integration():
     cols = columns()
     assert "race" in cols
+     
+
+def test_unique_terms():
+    terms = unique_terms('sex')
+    assert "female" in terms
