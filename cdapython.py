0bc3c989a49d7eb75ced6203b9bfa8d8b98c1605
fkaufman-asym <fkaufman@asymmetrik.com>
2021-06-29 09:16:31 -0400

    review comments

--- cdapython.py
+++ cdapython.py
@@ -172,22 +172,3 @@ def unique_terms(col_name, system=None, version=table_version, host=CDA_API_URL)
         query_result = get_query_result(api_instance, api_response.query_id, 0, 1000)
         return [list(t.values())[0] for t in query_result]
 
-
-# column ->
-# SELECT DISTINCT(column)
-
-# D.column ->
-# SELECT DISTINCT(_D.column) FROM TABLE, UNNEST(D) AS _D
-
-# A.B.C.D.column ->
-# SELECT DISTINCT(_D.column) FROM TABLE, UNNEST(A) AS _A, UNNEST(_A.B) AS _B, UNNEST(_B.C) AS _C, UNNEST(_C.D) AS _D
-def _get_unnest_clause(col_name):
-    _new_col, _unnest = col_name, []
-    c = col_name.split(".")
-    if len(c) > 1:
-        _new_col = f"_{c[-2]}.{c[-1]}"
-        _unnest = [f"UNNEST({c[0]}) AS _{c[0]}"]
-        for n in range(1, len(c) - 1):
-            _unnest += [f"UNNEST(_{c[n - 1]}.{c[n]}) AS _{c[n]}"]
-
-    return _new_col, _unnest
