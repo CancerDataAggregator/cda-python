9c2923deb6c6564106b85337c739040305c2592f
Frederick Kaufman <84089414+fkaufman-asym@users.noreply.github.com>
2021-06-30 10:29:23 -0400

    Bugfix/issue26 support system in uniq values (#31)
    * Preserve System value and use native API uniqueValues endpoint
    
    * Call uniqueValues API directly
    
    * revert endpoint to prod
    
    * Update version number
    
    * add really basic integration test for unique_terms
    
    * review comments
    
    * revert notebook
    
    * remove unnest_clause test
    
    * fix unique_values test

--- cdapython.py
+++ cdapython.py
@@ -6,7 +6,7 @@ import cda_client
 from cda_client.api.query_api import QueryApi
 from cda_client.model.query import Query
 
-__version__ = "2021.6.15"
+__version__ = "2021.6.28"
 
 CDA_API_URL = "https://cda.cda-dev.broadinstitute.org"
 table_version = "v3"
@@ -166,46 +166,9 @@ def unique_terms(col_name, system=None, version=table_version, host=CDA_API_URL)
             configuration=cda_client.Configuration(host=host)
     ) as api_client:
         api_instance = QueryApi(api_client)
-        _new_col, _unnest_col = _get_unnest_clause(col_name=col_name)
-
-        if system:
-            _new_system, _unnest_system = _get_unnest_clause(col_name='ResearchSubject.identifier.system')
-            _unnest = _unnest_col + [item for item in _unnest_system if item not in _unnest_col]
-            _where_clause = f" WHERE {_new_system}=\"{system}\""
-        else:
-            _unnest = _unnest_col
-            _where_clause = ''
-        if _unnest:
-            _unnest_clause = f", {','.join(_unnest)}"
-        else:
-            _unnest_clause = ''
-
-        query = f"SELECT DISTINCT({_new_col}) FROM `gdc-bq-sample.cda_mvp.{version}`" \
-                f"{_unnest_clause}{_where_clause} ORDER BY {_new_col}"
-
-        sys.stderr.write(f"{query}\n")
 
         # Execute query
-        api_response = api_instance.sql_query(query)
+        api_response = api_instance.unique_values(version=version, body=col_name, system=system)
         query_result = get_query_result(api_instance, api_response.query_id, 0, 1000)
         return [list(t.values())[0] for t in query_result]
 
-
-# column ->
-# SELECT DISTINCT(column)
-
-# D.column ->
-# SELECT DISTINCT(_D.column) FROM TABLE, UNNEST(D) AS _D
-
-# A.B.C.D.column ->
-# SELECT DISTINCT(_D.column) FROM TABLE, UNNEST(A) AS _A, UNNEST(_A.B) AS _B, UNNEST(_B.C) AS _C, UNNEST(_C.D) AS _D
-def _get_unnest_clause(col_name):
-    _new_col, _unnest = col_name, []
-    c = col_name.split(".")
-    if len(c) > 1:
-        _new_col = f"_{c[-2]}.{c[-1]}"
-        _unnest = [f"UNNEST({c[0]}) AS _{c[0]}"]
-        for n in range(1, len(c) - 1):
-            _unnest += [f"UNNEST(_{c[n - 1]}.{c[n]}) AS _{c[n]}"]
-
-    return _new_col, _unnest
