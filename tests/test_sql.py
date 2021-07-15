793caab75ab0034ab89219986740ad08ea84c55c
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-07-15 10:48:59 -0400

    added a unit test

---
+++ tests/test_sql.py
@@ -0,0 +1,6 @@
+
+from cdapython import Q
+
+def test():
+    qr = Q.sql("SELECT * FROM `gdc-bq-sample.cda_mvp.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS` WHERE table_name = 'v3' Limit 5")
+    assert qr[0]["table_schema"] == 'cda_mvp'
