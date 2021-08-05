6b19d9b2b79608c578da0367fa47eb45003f46f4
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-08-05 14:41:08 -0400

    update test to requirements.txt

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     python_requires='>=3.6',
     install_requires=[
         "cda-client@git+https://github.com/CancerDataAggregator/cda-service-python-client.git",
-        "python-dotenv==0.18.0"
+        "python-dotenv"
     ],
     description='User friendly Python library to access CDA service.',
     long_description=long_description,
