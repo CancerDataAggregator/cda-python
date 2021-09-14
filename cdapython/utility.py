4bc606d1eb09a1d80eedfd9c7a6ff565e47bb195
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-09-14 14:17:30 -0400

    updated columns to take parms

--- cdapython/utility.py
+++ cdapython/utility.py
@@ -43,7 +43,7 @@ def unique_terms(col_name: str, system: str = "", limit: int = 1000):
             # Execute query
             query_result = get_query_result(
                 api_instance, api_response.query_id, 0, limit
-            )
+            ) 
             uniqueArray = np.array([list(t.values())[0] for t in query_result])
             return uniqueArray.tolist()
     except ServiceException as httpError:
@@ -57,9 +57,12 @@ def columns(
     version: Optional[str] = table_version,
     host: Optional[str] = None,
     limit: int = 1000,
+    table: str = const.default_table.split(".")[1]
 ):
+    if table not in ["cda_mvp", "integration"]:
+        raise ValueError('Table is not in white list')
 
-    query = f"SELECT field_path FROM `gdc-bq-sample.cda_mvp.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS` WHERE table_name = '{version}'"
+    query = f"SELECT field_path FROM `gdc-bq-sample.{table}.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS` WHERE table_name = '{version}'"
     sys.stderr.write(f"{query}\n")
     try:
         # Execute query
