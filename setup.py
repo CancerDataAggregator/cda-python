be76678fbb85d06ea1978637a746bd51796463e9
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-10-21 13:00:11 -0400

    testing setup.py file copy

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ __version__ = getVersion(".env")
 print(__version__)
 current_path = Path(__file__).parent
 
-
+data = open(".env","r").read()
+writeoutEnv = open("cdapython/.env","w").write(data)
 name = "cdapython"
 version = __version__
 now = datetime.utcnow()
