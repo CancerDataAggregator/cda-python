8e70b0bc43fb87af44afcc1bd5d44aa8190d0737
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-10-22 10:34:52 -0400

    check path loaded

--- cdapython/constantVariables.py
+++ cdapython/constantVariables.py
@@ -4,9 +4,15 @@ from typing import Optional
 
 from dotenv import load_dotenv
 
-load_dotenv()
 env_path = Path(".") / ".env"
-load_dotenv(dotenv_path=env_path)
+if env_path.exists is True():
+    load_dotenv(dotenv_path=env_path)
+else:
+    from os.path import join, dirname
+
+    dotenv_path = join(dirname(__file__), ".env")
+    load_dotenv(dotenv_path)
+
 
 __version__: Optional[str] = os.getenv("VERSION")
 CDA_API_URL: Optional[str] = os.getenv("CDA_API_URL")
