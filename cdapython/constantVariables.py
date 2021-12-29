7eaf8fdf3602d6c923a0d1e389300d8617d146a5
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-12-29 09:27:39 -0500

    removed debug mode in constVars and updated verison

--- cdapython/constantVariables.py
+++ cdapython/constantVariables.py
@@ -1,28 +1,12 @@
-import os
 
-VERSION = "2021.12.8"
+VERSION = "2021.12.29"
 DATABASETABLE_VERSION = "all_v2"
 DATABASETABLE = "gdc-bq-sample.integration"
 CDA_API_URL_ENV = "https://cda.cda-dev.broadinstitute.org"
 
 
-val = os.environ.get("CDAPYTHON_ENV")
-
-if val is not "development":
-    __version__: str = VERSION
-    CDA_API_URL: str = CDA_API_URL_ENV
-    table_version: str = DATABASETABLE_VERSION
-    default_table: str = DATABASETABLE
-    project_name: str = default_table.split(".")[0]
-else:
-    print("DEBUG MODE")
-
-    DATABASETABLE_VERSION = "all_v1"
-    DATABASETABLE = "gdc-bq-sample.dev"
-    CDA_API_URL_ENV = "http://34.71.0.127:8080"
-
-    __version__: str = VERSION
-    CDA_API_URL: str = CDA_API_URL_ENV
-    table_version: str = DATABASETABLE_VERSION
-    default_table: str = DATABASETABLE
-    project_name: str = default_table.split(".")[0]
+__version__: str = VERSION
+CDA_API_URL: str = CDA_API_URL_ENV
+table_version: str = DATABASETABLE_VERSION
+default_table: str = DATABASETABLE
+project_name: str = default_table.split(".")[0]
--- tests/test_pythonVerison.py
+++ tests/test_pythonVerison.py
@@ -1,7 +1,7 @@
 from cdapython import __version__
 
 
-version = "2021.12.8"
+version = "2021.12.29"
 
 
 def test_python_version() -> None:
