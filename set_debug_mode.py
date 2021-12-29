0bc2fa8a53274c818db233b2a1436cf7916c8259
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-12-29 09:29:51 -0500

    forgot to check debug script

--- set_debug_mode.py
+++ set_debug_mode.py
@@ -4,11 +4,7 @@ answer = (
     str(input("Would you like to turn on debug mode?\ntype yes or no ")).lower().strip()
 )
 
-if answer == "yes" or answer == "y":
-    os. = "development"
-else:
-    del os.environ["CDAPYTHON_ENV"]
-
-
-
-unset CDAPYTHON_ENV 
+# if answer == "yes" or answer == "y":
+#     os = "development"
+# else:
+#     del os.environ["CDAPYTHON_ENV"]
