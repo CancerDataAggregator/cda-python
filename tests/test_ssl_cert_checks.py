b9f7e4e218f0b306ee68ed28f6d18753f734b3b7
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-01-24 16:01:02 -0500

    forgot to remove test function

--- tests/test_ssl_cert_checks.py
+++ tests/test_ssl_cert_checks.py
@@ -1,5 +1,6 @@
 from cdapython import Q
 
+
 def test_ssl_sql():
     qr = Q.sql(
         "SELECT * FROM `gdc-bq-sample.cda_mvp.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS` WHERE table_name = 'v3' Limit 5",
@@ -9,9 +10,6 @@ def test_ssl_sql():
 
 
 def test_ssl_Q():
-    q = Q('id = "TCGA-13-1409"') 
+    q = Q('id = "TCGA-13-1409"')
     r = q.run(verify=False)
     assert r.count == 1
-
-
-test_ssl_Q()
