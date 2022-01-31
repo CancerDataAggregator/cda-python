6129282162fdcfeb1503746a32cadacbf332164f
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-01-31 10:51:06 -0500

    Forgot to import conversion map from merge

--- cdapython/Q.py
+++ cdapython/Q.py
@@ -4,6 +4,7 @@ import logging
 from multiprocessing.pool import ApplyResult
 from typing import Optional, List
 from urllib3.exceptions import InsecureRequestWarning, SSLError
+from cdapython.ConvertionMap import convertionMap
 from cdapython.Result import Result, get_query_result
 from cdapython.functions import col, quoted, unquoted
 from cda_client.api.meta_api import MetaApi
