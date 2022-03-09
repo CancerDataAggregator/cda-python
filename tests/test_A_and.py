a52c06145f55f6172de994a440d92b4114b1a323
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-03-09 15:03:42 -0500

    removing localhost from run

--- tests/test_A_and.py
+++ tests/test_A_and.py
@@ -7,7 +7,7 @@ def test_and_op():
     q2 = Q('ResearchSubject.associated_project = "TCGA-OV"')
 
     q = q1.And(q2)
-    r = q.run(host="http://localhost:8080")
+    r = q.run()
     print(r)
 
     assert isinstance(r.count, int) is True
