98349b6295262de41ff95cd4f6baac16cbdc2471
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-03-21 14:14:27 -0400

    removed print

--- tests/global_settings.py
+++ tests/global_settings.py
@@ -3,5 +3,4 @@ from cdapython.constantVariables import CDA_API_URL
 localhost = "http://localhost:8080"
 production_host = CDA_API_URL
 integration_host = "http://35.192.60.10:8080/"
-host = localhost
-print(host)
+host = integration_host
