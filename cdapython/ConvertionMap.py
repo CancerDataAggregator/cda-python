34b9a623a0c904c815f79ce34d6f5acff07721b4
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-03-21 15:01:55 -0400

    Error with typechecking

--- cdapython/ConvertionMap.py
+++ cdapython/ConvertionMap.py
@@ -1,7 +1,8 @@
+from typing import Dict
 """
 This dictionary is made for updating name changes in the schema this is a way to keep backwards compatibility for the user and hopefully inform them to use the new names
 """
-convertionMap: dict[str, str] = {
+convertionMap: Dict[str, str] = {
     "ResearchSubject.associated_project": "ResearchSubject.member_of_research_project",
     "ResearchSubject.primary_disease_type": "ResearchSubject.primary_diagnosis_condition",
     "ResearchSubject.primary_disease_site": "ResearchSubject.primary_diagnosis_site",
@@ -17,5 +18,5 @@ convertionMap: dict[str, str] = {
     "File.associated_project": "associated_project",
     "File.drs_uri": "drs_uri",
     "File.byte_size": "byte_size",
-    "File.checksum": "checksum",
+    "File.checksum": "checksum"
 }
