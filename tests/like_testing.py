75c371e84fc214e01e9b0cda5c260fd1937f7ed8
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-03-22 14:30:55 -0400

    forgot to remove

--- tests/like_testing.py
+++ tests/like_testing.py
@@ -4,13 +4,14 @@ from tests.global_settings import host
 
 def testing_like():
     q = query(
-        'ResearchSubject.primary_disease_type LIKE "Lung%" AND ResearchSubject.id = "P012"'
+        'ResearchSubject.primary_disease_type LIKE "Lung_%" AND ResearchSubject.id = "P012"'
     )
     # print(q)
     # r = q.run(host="http://localhost:8080")
     # c = q.counts(host="http://localhost:8080")
     v = Q('ResearchSubject.primary_disease_type = "Lung%"')
     v.run(host=host)
+    print(v)
 
 
 testing_like()
