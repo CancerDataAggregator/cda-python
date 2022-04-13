7b9dde1f55b6faee73b1a97f291f96e879a11173
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-04-13 13:16:49 -0400

    add host

--- tests/count_wrong.py
+++ tests/count_wrong.py
@@ -1,6 +1,7 @@
 from cdapython import Q
-from global_settings import localhost
-q = Q('id = "TCGA-E2-A10A"') # note the double quotes for the string value
+from global_settings import host
 
-r = q.run(host=localhost)
+q = Q('id = "TCGA-E2-A10A"')  # note the double quotes for the string value
+
+r = q.run(host=host)
 print(r)
