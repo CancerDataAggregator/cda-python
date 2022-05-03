8ebc56ace5ca3ecf7a4f2a31d997ab2dac3e2dc0
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-05-03 13:52:35 -0400

    dict need to change

--- tests/test_checking_IN.py
+++ tests/test_checking_IN.py
@@ -7,7 +7,7 @@ def checking_test():
         "ResearchSubject.id IN ['C0EF0C13-3109-47CF-9BA4-076AB7EB7660',' 6AA44F89-FCE7-46FE-A1CB-874CD5EFA4A4']"
     ).And(Q('sex = "male"'))
     print(q1)
-    assert q1.query.to_dict()["l"] == "IN"
+    assert q1.query.to_dict()["l"]["node_type"] == "IN"
     r = q1.run(host=host)
 
 
