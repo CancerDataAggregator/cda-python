f0aa39158d676fe847a024eef85a5055a4c4b6c8
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-05-16 16:17:18 -0400

    update test

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,13 +1,13 @@
 from cdapython import Q, columns
 from cdapython.utility import unique_terms
-from tests.global_settings import localhost
+from tests.global_settings import localhost, host
 
 # q = Q('id != "TCGA-13-1409"').run()
 # print(q.to_list())  # note the double quotes for the string value
 # print(q.to_dataframe())
 # print(columns(host=localhost))
-print(columns(host=localhost).to_dataframe())
-print(unique_terms("ResearchSubject.Diagnosis.stage", host=localhost, show_sql=True))
+print(columns(host=host).to_dataframe())
+print(unique_terms("ResearchSubject.Diagnosis.stage", host=host, show_sql=True))
 # print(columns(host=localhost))
 # r = q.run(host=localhost, verbose=False)
 # df = r.to_dataframe()
