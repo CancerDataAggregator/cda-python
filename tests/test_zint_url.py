f757cf306646ace7dd7094f83b59cb678c24afcd
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-05-23 13:12:28 -0400

    updated test

--- tests/test_zint_url.py
+++ tests/test_zint_url.py
@@ -4,4 +4,4 @@ from tests.global_settings import host
 
 def test_url_change():
     Q.set_host_url(host)
-    assert Q.get_host_url().split(":")[2] == "8080"
+    assert Q.get_host_url().split(":")[2] == "8080/"
