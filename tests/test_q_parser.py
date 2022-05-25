7b201db5183fa6a0f95e7cddca40e9aad4e76663
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-05-25 11:49:45 -0400

    updated test

--- tests/test_q_parser.py
+++ tests/test_q_parser.py
@@ -1,12 +1,11 @@
-from cdapython.Q import Q
-from cdapython.utility import query
+from cdapython import Q, query
 
 
 def test_parser() -> None:
     qc2 = query(
-        'ResearchSubject.Diagnosis.tumor_stage = "Stage IIIC" OR  ResearchSubject.Diagnosis.tumor_stage = "Stage IV" '
+        'ResearchSubject.Diagnosis.tumor_stage = "Stage IIIC" OR  ResearchSubject.Diagnosis.tumor_stage = "Stage IV"'
     )
-
+    print(qc2.to_json())
     assert isinstance(qc2, Q)
     assert qc2.query.to_dict()["node_type"] == "OR"
 
