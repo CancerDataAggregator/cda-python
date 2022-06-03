992a7947b04aa85951345026383cbbbf5806cca1
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-06-03 09:09:52 -0400

    dot import

--- cdapython/results/__init__.py
+++ cdapython/results/__init__.py
@@ -1,3 +1,3 @@
-from .result import Result, get_query_result
-from .count_result import CountResult
-from .string_result import StringResult, get_query_string_result
+from . import Result, get_query_result
+from . import CountResult
+from . import StringResult, get_query_string_result
