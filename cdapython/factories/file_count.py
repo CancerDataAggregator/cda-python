93e3741b39013eeccbca1a3d9c8880445c3e7ae8
rfricke-asymmetrik <rfricke@asymmetrik.com>
2022-06-21 18:19:51 -0400

    return count result from file_count

--- cdapython/factories/file_count.py
+++ cdapython/factories/file_count.py
@@ -1,8 +1,12 @@
+from typing import Optional
 from cda_client.api.query_api import QueryApi
 from cda_client.api_client import Endpoint
 from cda_client.model.query import Query
 from cdapython.factories.entity import Entity
 from cdapython.factories.q_factory import AbstractFactory
+from cdapython.results.result import Result
+from cdapython.results.count_result import CountResult
+from cda_client.model.query_response_data import QueryResponseData
 
 
 class FileCount(Entity):
@@ -17,6 +21,28 @@ class FileCount(Entity):
     ) -> Endpoint:
         return api_instance.file_counts_query(
             query, version=version, dry_run=dry_run, table=table, async_req=async_req
+        )
+
+    def _build_result_object(
+        self,
+        api_response: QueryResponseData,
+        query_id: str,
+        offset: Optional[int],
+        limit: Optional[int],
+        api_instance: QueryApi,
+        show_sql: bool,
+        show_count: bool,
+        format_type: str = "json",
+    ) -> Result:
+        return CountResult(
+            api_response,
+            query_id,
+            offset,
+            limit,
+            api_instance,
+            show_sql,
+            show_count,
+            format_type,
         )
 
     class Factory(AbstractFactory):
