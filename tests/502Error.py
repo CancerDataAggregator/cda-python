c918ee8d1784a226e090a198964db0e06c701c3f
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-06-22 10:48:35 -0400

    updated test

--- tests/502Error.py
+++ tests/502Error.py
@@ -1,11 +1,47 @@
-import json
-
 from cdapython import Q
+from tests.global_settings import localhost, host
 
-q1 = Q("ResearchSubject.Diagnosis.age_at_diagnosis > 50*365")
-q2 = Q('ResearchSubject.associated_project = "TCGA-OV"')
-q = q1.AND(q2)
+# q1 = Q("ResearchSubject.Diagnosis.age_at_diagnosis > 50*365")
+# q2 = Q('ResearchSubject.associated_project = "TCGA-OV"').file
+# a = q2.AND(q1)
 
-data = q.researchsubject.count.run(host="http://localhost:8080")
 
-print(data)
+# a = Q(
+#     'ResearchSubject.primary_diagnosis_site IN ("uterus", "uterus, NOS", "Cervix uteri", "Chest-Abdomen-Pelvis, Leg, TSpine")'
+# )
+
+# t = Q(
+#     "ResearchSubject.primary_diagnosis_site =  'Bones, joints and articular cartilage of other and unspecified sites' "
+# )
+# print(a.to_json())
+# Q("ResearchSubject.Diagnosis.morphology = '84%'").researchsubject.run(
+#     host="http://localhost:8080"
+# ).to_dict()
+
+
+d = Q(
+    """
+    ResearchSubject.primary_diagnosis_site = "uterus, NOS" OR 
+    ResearchSubject.primary_diagnosis_site = "uterus" OR 
+    ResearchSubject.primary_diagnosis_site = "Cervix" OR 
+    ResearchSubject.primary_diagnosis_site = "Cervix uteri"
+"""
+).researchsubject.count.run(host=host)
+
+
+Q(
+    """
+      ResearchSubject.primary_diagnosis_site = "uterus, NOS" OR 
+      ResearchSubject.primary_diagnosis_site = "uterus" OR 
+      ResearchSubject.primary_diagnosis_site = "Cervix" OR 
+      ResearchSubject.primary_diagnosis_site = "Cervix uteri
+      "
+"""
+).researchsubject.count.run(show_sql=True)
+print(d)
+# print(a.researchsubject.count.run(host=localhost))
+# q = q1.AND(q2)
+
+# data = q.researchsubject.count.run(host="http://localhost:8080")
+
+# print(data)
