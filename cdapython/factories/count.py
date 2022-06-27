ee83f70fd932949de8b92c7934c26db522b3424e
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-06-27 13:51:11 -0400

    counts file fix

--- cdapython/factories/count.py
+++ cdapython/factories/count.py
@@ -1,10 +1,14 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 from cda_client.api.query_api import QueryApi
 from cda_client.api_client import Endpoint
 from cda_client.model.query import Query
 from cdapython.factories.entity import Entity
 from cdapython.factories import FILE_COUNT
 from cdapython.factories.q_factory import AbstractFactory, QFactory
+from cda_client.model.query_response_data import QueryResponseData
+
+from cdapython.results.count_result import CountResult
+from cdapython.results.result import Result
 
 if TYPE_CHECKING:
     from cdapython.Q import Q
@@ -26,6 +30,28 @@ class Count(Entity):
     ) -> Endpoint:
         return api_instance.global_counts(
             query, version=version, dry_run=dry_run, table=table, async_req=async_req
+        )
+
+    def _build_result_object(
+        self,
+        api_response: QueryResponseData,
+        query_id: str,
+        offset: Optional[int],
+        limit: Optional[int],
+        api_instance: QueryApi,
+        show_sql: bool,
+        show_count: bool,
+        format_type: str = "json",
+    ) -> Result:
+        return CountResult(
+            api_response,
+            query_id,
+            offset,
+            limit,
+            api_instance,
+            show_sql,
+            show_count,
+            format_type,
         )
 
     class Factory(AbstractFactory):
