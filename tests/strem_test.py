8b291f053fe3f3b5adb3386e5629c99355efb21f
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-06-29 17:17:37 -0400

    black error

--- tests/strem_test.py
+++ tests/strem_test.py
@@ -1,9 +1,13 @@
 from pandas import DataFrame, concat
 
 from cdapython import Q
-from tests.global_settings import host, localhost,table
+from tests.global_settings import host, localhost, table
 
-q = Q('ResearchSubject.primary_disease_type LIKE "Lung%"').run(host=host,table=table).to_dataframe()
+q = (
+    Q('ResearchSubject.primary_disease_type LIKE "Lung%"')
+    .run(host=host, table=table)
+    .to_dataframe()
+)
 
 print(q)
 # box = []
