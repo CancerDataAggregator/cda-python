3e3e4d37f746611e783954b508f7d5914eb401cb
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-06-29 11:54:42 -0400

    test

--- tests/strem_test.py
+++ tests/strem_test.py
@@ -3,10 +3,11 @@ from pandas import DataFrame, concat
 from cdapython import Q
 from tests.global_settings import host, localhost
 
-q = Q('ResearchSubject.primary_disease_type LIKE "Lung%"').run(host=host)
+q = Q('ResearchSubject.primary_disease_type LIKE "Lung%"').run(host=host).to_dataframe()
 
-box = []
-for i in q.paginator(to_list=True):
-    box.extend(i)
+print(q)
+# box = []
+# for i in q.paginator(to_list=True):
+#     box.extend(i)
 
-print(len(box))
+# print(len(box))
