b06c069499e431efb99f30521862af5bf26ccbc7
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-06-29 17:27:29 -0400

    linit error

--- tests/test_ssl_cert_checks.py
+++ tests/test_ssl_cert_checks.py
@@ -1,11 +1,11 @@
 from cdapython import Q, columns
-from tests.global_settings import host,table
+from tests.global_settings import host, table
 
 
 def test_ssl_q():
     q = Q('id = "TCGA-13-1409"')
-    r = q.run(verify=False, host=host,table=table)
+    r = q.run(verify=False, host=host, table=table)
     assert r.count == 1
 
 
-columns(verify=False, host=host,table=table)
+columns(verify=False, host=host, table=table)
