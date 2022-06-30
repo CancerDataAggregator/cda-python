0f23e5002b7fa18168dfd1f3efe9e4044fe4214a
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-06-30 13:40:25 -0400

    liniting

--- tests/test_unique_version.py
+++ tests/test_unique_version.py
@@ -1,4 +1,4 @@
 from cdapython import unique_terms
-from tests.global_settings import host,table
+from tests.global_settings import host, table
 
-print(unique_terms("vital_status", host=host,table=table))
+print(unique_terms("vital_status", host=host, table=table))
