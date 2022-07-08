0515e79fa9170a85d5a02e2285ecb39729985c9c
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-07-08 11:06:32 -0400

    changing urls for prod

--- cdapython/constant_variables.py
+++ cdapython/constant_variables.py
@@ -3,7 +3,7 @@ class Constants:
     _DATABASETABLE_VERSION: str = "all_Subjects_v3_0_final"
     _CLIENT_VERSION: str = "3.0.0"
     _DATABASETABLE: str = "broad-dsde-prod.cda_prod"
-    _CDA_API_URL_ENV: str = "https://cancerdata.dsde-prod.broadinstitute.org/"
+    _CDA_API_URL_ENV: str = "https://cda.datacommons.cancer.gov/"
     _DATABASETABLE_FOR_FILES: str = "broad-dsde-prod.cda_prod"
     _DATABASETABLE_VERSION_FOR_FILES: str = "all_Files_v3_0_final"
 
