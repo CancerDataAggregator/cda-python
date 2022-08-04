18184ac2c78e626cded3907566ba09eaceec2044
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-08-04 13:50:55 -0400

    updating test

--- tests/test_unique_terms_check.py
+++ tests/test_unique_terms_check.py
@@ -4,9 +4,12 @@ from tests.global_settings import host, table, localhost
 
 def test_unique_terms_convert() -> None:
     d = unique_terms(
-        "species", host=localhost, table="gdc-bq-sample.dev", show_sql=True
-    )
-    print(d.to_list())
+        "species",
+        host="http://35.192.60.10:8080/",
+        table="gdc-bq-sample.dev",
+        show_sql=True,
+        show_counts=True,
+    ).to_dataframe()
 
 
 test_unique_terms_convert()
