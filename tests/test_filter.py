592e6f519073edf7678acf498156dd052104c7f9
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-09-02 13:18:20 -0400

    added changes to table

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,17 +1,17 @@
 from cdapython import Q, columns
 from cdapython import unique_terms
-from tests.global_settings import host, localhost, table
+from tests.global_settings import integration_host, integration_table
 
 # q = Q('id != "TCGA-13-1409"').run()
 # print(q.to_list())  # note the double quotes for the string value
 # print(q.to_dataframe())
 # print(columns(host=localhost))
-print(columns(host=host, table=table).to_dataframe())
-print(
-    unique_terms(
-        "ResearchSubject.Diagnosis.stage", host=host, table=table, show_sql=True
-    )
-)
+print(columns(host=integration_host, table=integration_table))
+# print(
+#     unique_terms(
+#         "ResearchSubject.Diagnosis.stage", host=host, table=table, show_sql=True
+#     )
+# )
 # print(columns(host=localhost))
 # r = q.run(host=localhost, verbose=False)
 # df = r.to_dataframe()
