dea5f3d565494501bec0d7ba4c7b8116fcc18bc4
JonnyDBR <112571723+JonnyDBR@users.noreply.github.com>
2022-09-02 13:25:03 -0400

    testing by Dion and Jon

--- tests/test_E_ssl.py
+++ tests/test_E_ssl.py
@@ -1,8 +1,47 @@
 from cdapython import Q
+from unittest import mock
+from pandas import DataFrame
+from tests.fake_result import FakeResultData
+from cdapython.results.result import Result
 
+result = [
+    {
+        'id': '09CO022',
+        'identifier': [
+            {'system': 'GDC', 'value': '09CO022'},
+            {'system': 'PDC', 'value': '09CO022'},
+            {'system': 'IDC', 'value': '09CO022'}
+        ],
+        'species': 'homo sapiens',
+        'sex': 'female',
+        'race': 'black or african american',
+        'ethnicity': 'not hispanic or latino',
+        'days_to_birth': None,
+        'subject_associated_project': ['CPTAC-2', 'cptac_coad'],
+        'vital_status': 'Not Reported',
+        'days_to_death': None,
+        'cause_of_death': None
+    }
+]
 
-def test_ssl():
+fake = FakeResultData(result)
+fake_result = Result(
+                    api_response=fake.api_response,
+                    query_id=fake.query_id,
+                    offset=fake.offset,
+                    limit=fake.limit,
+                    api_instance=fake.api_instance,
+                    show_sql=fake.show_sql,
+                    show_count=fake.show_count,
+                    format_type=fake.format_type,
+                )
+@mock.patch("cdapython.Q.run", return_value=fake_result)
+def test_ssl(a):
 
     q1 = Q('ResearchSubject.id = "c5421e34-e5c7-4ba5-aed9-146a5575fd8d"')
 
     q1.run(verify=False)
+
+    qsub = q1.subject.run()
+    assert isinstance(qsub.to_list(), list) is True
+    assert isinstance(qsub.to_dataframe(), DataFrame) is True
--- tests/test_set_new_host.py
+++ tests/test_set_new_host.py
@@ -1,9 +1,10 @@
 from cdapython import Q, columns
-from tests.global_settings import table, host
+from tests.global_settings import integration_host, integration_table
 
-Q.set_host_url(host)
-Q.set_default_project_dataset(table)
-Q.get_host_url()
-Q.get_default_project_dataset()
+def test_set_new_host():
+    Q.set_host_url(integration_host)
+    Q.set_default_project_dataset(integration_table)
+    Q.get_host_url()
+    Q.get_default_project_dataset()
 
-print(columns().to_list())
+    print(columns().to_list())
