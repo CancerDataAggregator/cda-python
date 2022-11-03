75ff86af064f51c4f81975dba825208e44dc67c7
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2022-11-03 12:11:15 -0400

    updating tests

--- tests/paging.py
+++ tests/paging.py
@@ -1,12 +1,12 @@
 from cdapython import Q
 from global_settings import integration_host, integration_table
 
-# a = (
-#     Q("subject_identifier_system = 'GDC'")
-#     .ORDER_BY("days_to_birth:-1")
-#     .subject.run(show_sql=True, host=integration_host, table=integration_table)
-#     .df_to_table()
-# )
-# print(a)
+a = (
+    Q("subject_identifier_system = 'GDC'")
+    .ORDER_BY("days_to_birth:-1")
+    .subject.run(show_sql=True, host=integration_host, table=integration_table)
+    .df_to_table()
+)
+print(a)
 
 Q.bigquery_status()
