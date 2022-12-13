d0ef4d344666275a22ac60c46be5b1c0126a1cba
Andrea Haessly <ahaessly@broadinstitute.org>
2022-12-13 11:48:27 -0500

    move file to temp

--- tests/test_Q_count.py
+++ tests/test_Q_count.py
@@ -6,7 +6,7 @@ from cdapython import Q
 from tests.global_settings import host
 
 sex = Q('sex = "female"')
-cancer = Q('ResearchSubject.primary_disease_type = "Breast Invasive Carcinoma"')
+cancer = Q('primary_disease_type = "Breast Invasive Carcinoma"')
 ageL = Q("days_to_birth <= -30*365")
 ageU = Q("days_to_birth >= -45 * 365")
 
