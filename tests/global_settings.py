c7397b5e8c397a03537a65f3b4a6235f830995c4
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-04-24 14:21:28 -0400

    updated tables

--- tests/global_settings.py
+++ tests/global_settings.py
@@ -2,7 +2,7 @@ from cdapython.constant_variables import Constants
 
 localhost = "http://localhost:8080"
 production_host = Constants.cda_api_url
-table_dev = "gdc-bq-sample.yet_another_sandbox"
+table_dev = "gdc-bq-sample.dev"
 table_prod = Constants.default_table
 integration_host = "http://35.192.60.10:8080/"
 integration_table = "gdc-bq-sample.dev"
