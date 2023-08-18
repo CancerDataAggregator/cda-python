6f5487571f1b7577d2980a659faa59ed92edf528
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-08-18 15:55:19 -0400

    added test to check primary_disease_type and file_formats of tsv and bam

--- tests/testing_tsv.py
+++ tests/testing_tsv.py
@@ -1,18 +1,29 @@
 from global_settings import integration_host, integration_table
 
 from cdapython import Q
+from cdapython.utils.utility import unique_terms
 
-r = Q("file_format = 'tsv'").AND(Q("SYMBOL LIKE 'TP53%'"))
+r = Q(
+    """
+    primary_disease_type LIKE '%Carcinoma%' 
+    AND file_format = 'tsv' 
+    OR file_format = 'BAM'
+    """
+)
 
 # q = r.research_subject
 # a = r.subject
 
-# x = q.run(host=host)
-q2 = r.file.run(show_sql=True, host=integration_host, table=integration_table)
 
-q3 = r.file.run(show_sql=True)
+print(unique_terms("primary_disease_type").to_dataframe())
+# # x = q.run(host=host)
+q2 = r.treatment.run(show_sql=True, host=integration_host, table=integration_table)
+
+q3 = r.treatment.run(show_sql=True)
 
 
+print(q2)
+print(q3)
 # print(x)
 
 assert q2.total_row_count == q3.total_row_count
