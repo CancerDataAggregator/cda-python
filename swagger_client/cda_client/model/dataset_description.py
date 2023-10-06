9419fd1a63aaa2490a32f2f283830bee05698a5b
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-10-06 15:54:54 -0400

    adeed swagger_client setup.py for github action install

--- swagger_client/cda_client/model/dataset_description.py
+++ swagger_client/cda_client/model/dataset_description.py
@@ -11,13 +11,11 @@
 import re  # noqa: F401
 import sys  # noqa: F401
 
-from cda_client.exceptions import ApiAttributeError
 from cda_client.model_utils import (  # noqa: F401
     ApiTypeError,
     ModelComposed,
     ModelNormal,
     ModelSimple,
-    OpenApiModel,
     cached_property,
     change_keys_js_to_python,
     convert_js_args_to_python_args,
@@ -26,15 +24,16 @@ from cda_client.model_utils import (  # noqa: F401
     file_type,
     none_type,
     validate_get_composed_info,
+    OpenApiModel
 )
+from cda_client.exceptions import ApiAttributeError
 
 
 def lazy_import():
     from cda_client.model.dataset_info import DatasetInfo
     from cda_client.model.model import Model
-
-    globals()["DatasetInfo"] = DatasetInfo
-    globals()["Model"] = Model
+    globals()['DatasetInfo'] = DatasetInfo
+    globals()['Model'] = Model
 
 
 class DatasetDescription(ModelNormal):
@@ -61,9 +60,11 @@ class DatasetDescription(ModelNormal):
           as additional properties values.
     """
 
-    allowed_values = {}
+    allowed_values = {
+    }
 
-    validations = {}
+    validations = {
+    }
 
     @cached_property
     def additional_properties_type():
@@ -72,17 +73,7 @@ class DatasetDescription(ModelNormal):
         of type self, this must run after the class is loaded
         """
         lazy_import()
-        return (
-            bool,
-            date,
-            datetime,
-            dict,
-            float,
-            int,
-            list,
-            str,
-            none_type,
-        )  # noqa: E501
+        return (bool, date, datetime, dict, float, int, list, str, none_type,)  # noqa: E501
 
     _nullable = False
 
@@ -98,26 +89,28 @@ class DatasetDescription(ModelNormal):
         """
         lazy_import()
         return {
-            "release_date": (str,),  # noqa: E501
-            "cda_version": (str,),  # noqa: E501
-            "cda_model": (Model,),  # noqa: E501
-            "notes": (str,),  # noqa: E501
-            "datasets": ([DatasetInfo],),  # noqa: E501
+            'release_date': (str,),  # noqa: E501
+            'cda_version': (str,),  # noqa: E501
+            'cda_model': (Model,),  # noqa: E501
+            'notes': (str,),  # noqa: E501
+            'datasets': ([DatasetInfo],),  # noqa: E501
         }
 
     @cached_property
     def discriminator():
         return None
 
+
     attribute_map = {
-        "release_date": "release-date",  # noqa: E501
-        "cda_version": "cda-version",  # noqa: E501
-        "cda_model": "cda-model",  # noqa: E501
-        "notes": "notes",  # noqa: E501
-        "datasets": "datasets",  # noqa: E501
+        'release_date': 'release-date',  # noqa: E501
+        'cda_version': 'cda-version',  # noqa: E501
+        'cda_model': 'cda-model',  # noqa: E501
+        'notes': 'notes',  # noqa: E501
+        'datasets': 'datasets',  # noqa: E501
     }
 
-    read_only_vars = {}
+    read_only_vars = {
+    }
 
     _composed_schemas = {}
 
@@ -164,11 +157,11 @@ class DatasetDescription(ModelNormal):
             datasets ([DatasetInfo]): [optional]  # noqa: E501
         """
 
-        _check_type = kwargs.pop("_check_type", True)
-        _spec_property_naming = kwargs.pop("_spec_property_naming", True)
-        _path_to_item = kwargs.pop("_path_to_item", ())
-        _configuration = kwargs.pop("_configuration", None)
-        _visited_composed_classes = kwargs.pop("_visited_composed_classes", ())
+        _check_type = kwargs.pop('_check_type', True)
+        _spec_property_naming = kwargs.pop('_spec_property_naming', True)
+        _path_to_item = kwargs.pop('_path_to_item', ())
+        _configuration = kwargs.pop('_configuration', None)
+        _visited_composed_classes = kwargs.pop('_visited_composed_classes', ())
 
         self = super(OpenApiModel, cls).__new__(cls)
 
@@ -178,8 +171,7 @@ class DatasetDescription(ModelNormal):
                     kwargs.update(arg)
                 else:
                     raise ApiTypeError(
-                        "Invalid positional arguments=%s passed to %s. Remove those invalid positional arguments."
-                        % (
+                        "Invalid positional arguments=%s passed to %s. Remove those invalid positional arguments." % (
                             args,
                             self.__class__.__name__,
                         ),
@@ -195,27 +187,23 @@ class DatasetDescription(ModelNormal):
         self._visited_composed_classes = _visited_composed_classes + (self.__class__,)
 
         for var_name, var_value in kwargs.items():
-            if (
-                var_name not in self.attribute_map
-                and self._configuration is not None
-                and self._configuration.discard_unknown_keys
-                and self.additional_properties_type is None
-            ):
+            if var_name not in self.attribute_map and \
+                        self._configuration is not None and \
+                        self._configuration.discard_unknown_keys and \
+                        self.additional_properties_type is None:
                 # discard variable.
                 continue
             setattr(self, var_name, var_value)
         return self
 
-    required_properties = set(
-        [
-            "_data_store",
-            "_check_type",
-            "_spec_property_naming",
-            "_path_to_item",
-            "_configuration",
-            "_visited_composed_classes",
-        ]
-    )
+    required_properties = set([
+        '_data_store',
+        '_check_type',
+        '_spec_property_naming',
+        '_path_to_item',
+        '_configuration',
+        '_visited_composed_classes',
+    ])
 
     @convert_js_args_to_python_args
     def __init__(self, *args, **kwargs):  # noqa: E501
@@ -259,11 +247,11 @@ class DatasetDescription(ModelNormal):
             datasets ([DatasetInfo]): [optional]  # noqa: E501
         """
 
-        _check_type = kwargs.pop("_check_type", True)
-        _spec_property_naming = kwargs.pop("_spec_property_naming", False)
-        _path_to_item = kwargs.pop("_path_to_item", ())
-        _configuration = kwargs.pop("_configuration", None)
-        _visited_composed_classes = kwargs.pop("_visited_composed_classes", ())
+        _check_type = kwargs.pop('_check_type', True)
+        _spec_property_naming = kwargs.pop('_spec_property_naming', False)
+        _path_to_item = kwargs.pop('_path_to_item', ())
+        _configuration = kwargs.pop('_configuration', None)
+        _visited_composed_classes = kwargs.pop('_visited_composed_classes', ())
 
         if args:
             for arg in args:
@@ -271,8 +259,7 @@ class DatasetDescription(ModelNormal):
                     kwargs.update(arg)
                 else:
                     raise ApiTypeError(
-                        "Invalid positional arguments=%s passed to %s. Remove those invalid positional arguments."
-                        % (
+                        "Invalid positional arguments=%s passed to %s. Remove those invalid positional arguments." % (
                             args,
                             self.__class__.__name__,
                         ),
@@ -288,17 +275,13 @@ class DatasetDescription(ModelNormal):
         self._visited_composed_classes = _visited_composed_classes + (self.__class__,)
 
         for var_name, var_value in kwargs.items():
-            if (
-                var_name not in self.attribute_map
-                and self._configuration is not None
-                and self._configuration.discard_unknown_keys
-                and self.additional_properties_type is None
-            ):
+            if var_name not in self.attribute_map and \
+                        self._configuration is not None and \
+                        self._configuration.discard_unknown_keys and \
+                        self.additional_properties_type is None:
                 # discard variable.
                 continue
             setattr(self, var_name, var_value)
             if var_name in self.read_only_vars:
-                raise ApiAttributeError(
-                    f"`{var_name}` is a read-only attribute. Use `from_openapi_data` to instantiate "
-                    f"class with read only attributes."
-                )
+                raise ApiAttributeError(f"`{var_name}` is a read-only attribute. Use `from_openapi_data` to instantiate "
+                                     f"class with read only attributes.")
