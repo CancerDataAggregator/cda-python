0d17be02822437858bb41a5586d5995e253a553d
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-10-06 14:10:08 -0400

    updating test for muation_symbol to Title case

--- tests/test_mutation_symbol.py
+++ tests/test_mutation_symbol.py
@@ -2,9 +2,9 @@ from cdapython import Q
 from tests.global_settings import host, localhost, project
 
 
-# in postgres Symobol dose not Exist it is changed to Hugo_Symbol
+# in postgres Symbol dose not Exist it is changed to Hugo_Symbol
 def test_mutation_symbol():
-    symbol = Q("Hugo_Symbol LIKE 'TP53%'").run(host=host, show_sql=True)
+    symbol = Q("Symbol LIKE 'TP53%'").run(host=host, show_sql=True)
     print(symbol)
 
     assert len(symbol.to_list()) != 0
