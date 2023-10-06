0618591b23040db9c70e683406885a3fb3c01cd7
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-10-06 16:40:43 -0400

    Revert "update pyproject to latest black"
    This reverts commit 8818aac05c0716bf13bee3e8775b181a5231950a.

--- swagger_client/cda_client/api_client.py
+++ swagger_client/cda_client/api_client.py
@@ -147,6 +147,7 @@ class ApiClient(object):
             typing.List[typing.Dict[str, typing.Any]]
         ] = None,
     ):
+
         config = self.configuration
 
         # header parameters
@@ -912,6 +913,7 @@ class Endpoint(object):
         return self.callable(self, *args, **kwargs)
 
     def call_with_http_info(self, **kwargs):
+
         try:
             index = (
                 self.api_client.configuration.server_operation_index.get(
