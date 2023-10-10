dcbdc91c8d4d23af6351d489da6fe1a2827ff4e6
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-10-10 18:45:34 -0400

    pointing at localhost fix

--- tests/test_tumor.py
+++ tests/test_tumor.py
@@ -4,7 +4,7 @@ from tests.global_settings import host, integration_table, localhost
 
 def test_tumor():
     tp = Q("SYMBOL = 'TP53'").specimen.run(
-        show_sql=True, host=localhost, include_total_count=False
+        show_sql=True, host=host, include_total_count=False
     )
     assert len(tp.to_list()) > 0
 
