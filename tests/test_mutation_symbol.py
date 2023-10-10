03e1c873698218846cacf096356d8acf0237f48f
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-10-10 18:35:36 -0400

    turn off total count

--- tests/test_mutation_symbol.py
+++ tests/test_mutation_symbol.py
@@ -4,6 +4,6 @@ from tests.global_settings import host, localhost, project
 
 # in postgres Symbol dose not Exist it is changed to Hugo_Symbol
 def test_mutation_symbol():
-    symbol = Q("Hugo_Symbol LIKE 'TP53%'").run(host=host, show_sql=True)
+    symbol = Q("SYMBOL LIKE 'TP53%'").run(host=host, show_sql=True)
     print(symbol)
     assert len(symbol.to_list()) != 0
--- tests/test_tumor.py
+++ tests/test_tumor.py
@@ -1,4 +1,13 @@
-from cdapython import Q
-from tests.global_settings import host, integration_table
+from global_settings import host, integration_table, localhost
 
-print(Q("SYMBOL = 'TP53'").specimen.run(show_sql=True, host=host))
+from cdapython import Q
+
+
+def test_tumor():
+    tp = Q("SYMBOL = 'TP53'").specimen.run(
+        show_sql=True, host=localhost, include_total_count=False
+    )
+    assert len(tp.to_list()) > 0
+
+
+test_tumor()
