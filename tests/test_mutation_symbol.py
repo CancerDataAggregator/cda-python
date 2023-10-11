1d478d484e5d8e799440c5de8e42783176a2f3c7
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-10-11 09:16:22 -0400

    removed unused imports

--- tests/test_mutation_symbol.py
+++ tests/test_mutation_symbol.py
@@ -1,8 +1,7 @@
 from cdapython import Q
-from tests.global_settings import host, localhost, project
+from tests.global_settings import host
 
 
-# in postgres Symbol dose not Exist it is changed to Hugo_Symbol
 def test_mutation_symbol():
     symbol = Q("SYMBOL LIKE 'TP53%'").run(host=host, show_sql=True)
     print(symbol)
