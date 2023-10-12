40991fab593bdf15d9d215610a3271f51c50452c
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-10-12 17:17:48 -0400

    missspelling

--- tests/test_tumor.py
+++ tests/test_tumor.py
@@ -3,7 +3,7 @@ from tests.global_settings import host
 
 
 def test_tumor():
-    tp = Q("SYMOBL = 'TP53%'").specimen.run(
+    tp = Q("SYMBOL = 'TP53%'").specimen.run(
         show_sql=True, host=host, include_total_count=False
     )
     assert len(tp.to_list()) > 0
