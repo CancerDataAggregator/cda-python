594169e585094569c340d98f4f69e935aa14e8ae
Dion Boles <dion.boles@bluehalo.com>
2023-10-18 10:33:38 -0400

    fixed limit and offset that were not being set and fix code formatting and enable test.

--- tests/test_unique_get_all.py
+++ tests/test_unique_get_all.py
@@ -1,15 +1,14 @@
 import pytest
 
 from cdapython import columns, unique_terms
-from tests.global_settings import host, project
+from tests.global_settings import host
 
 
 # @pytest.mark.skip(reason="currently total row count not being returned CD-610")
 def test_gene_get_all():
-    genes = unique_terms("Gene").run(host=host,limit=2000)
+    genes = unique_terms("Gene").run(host=host, limit=100)
+    assert len(genes.to_dataframe()) == 100
 
-    assert len(genes.to_dataframe()) == 2000
-    
     all_genes = genes.get_all(limit=2000)
 
     assert all_genes.total_row_count == len(all_genes)
