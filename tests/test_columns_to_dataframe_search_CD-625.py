3c7865a4ac39758fe073bebe3fb03f5040499e7e
Dion Boles <dion.boles@bluehalo.com>
2023-10-30 17:12:11 -0400

    adjusted columns to match output of columns endpoint

---
+++ tests/test_columns_to_dataframe_search_CD-625.py
@@ -0,0 +1,17 @@
+from cdapython import columns
+from tests.global_settings import host
+
+
+def test_columns_dataframe_search():
+    df = columns(host=host).to_dataframe(
+        search_fields=["description", "fieldName"], search_value="diagnosis"
+    )
+    for _, row in df.iterrows():
+        df_output = row["description"].find("dog") > -1 or row["fieldName"].find("dog") > -1
+        assert df_output is False
+        df_output = row["description"].find("diagnosis") > -1 or row["fieldName"].find("diagnosis") > -1
+        assert df_output is True
+
+    assert "description" in df
+    assert "fieldName" in df
+    assert df is not None
