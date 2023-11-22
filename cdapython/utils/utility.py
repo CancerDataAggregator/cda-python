6f595e22295d22e6533f865e09ac0e9897c32ce5
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2023-11-22 13:24:23 -0500

    Updated unique terms to work with previous functionality, and fixed a  pagination bug (#221)
    * Updated unique terms to work with previous functionality, and fixed a pagination bug
    
    * updated test with new name and removed commented code
    
    * remove the test on remote
    
    * Updated descriptions for unique terms
    
    * updated show_counts to state that is nonfunctional

--- cdapython/utils/utility.py
+++ cdapython/utils/utility.py
@@ -17,7 +17,7 @@ from cdapython.constant_variables import Constants
 from cdapython.exceptions.custom_exception import HTTP_ERROR_API, HTTP_ERROR_SERVICE
 from cdapython.results.columns_result import ColumnsResult
 from cdapython.results.factories.collect_result import CollectResult
-from cdapython.results.page_result import get_query_result
+from cdapython.results.page_result import Paged_Result, get_query_result
 from cdapython.results.string_result import StringResult
 from cdapython.utils.Cda_Configuration import CdaConfiguration
 
@@ -101,15 +101,34 @@ def get_table_version() -> str:
 def unique_terms(
     col_name: str,
     system: str = "",
-) -> Q:
+    offset: int = 0,
+    host: Optional[str] = None,
+    verify: Optional[bool] = None,
+    async_req: Optional[bool] = True,
+    show_sql: bool = False,
+    show_counts: bool = False,
+    show_term_count: bool = False,
+    verbose: bool = True,
+    limit: int = 100,
+) -> Paged_Result:
+    # TODO : Should the value be changed? The "show_term_count" parameter was copied from Swagger-generated code. Is this description sufficient?
     """
     Show all unique terms for a given column.
     Args:
-        col_name (str): _description_
-        system (str, optional): _description_. Defaults to "".
+        col_name (str): This is the default way to search for a unique term from the CDA service API.
+        system (str, optional): his is an optional parameter used to filter the search values by data center, such as "GDC", "IDC", "PDC", or "CDS". Defaults to "".
+        offset (int, optional): The number of entries to skip. Defaults to 0.
+        host (Optional[str], optional): This is where the user can set a host for a different server. Defaults to None.
+        verify (Optional[bool], optional): This will send a request to the cda server without verifying the SSL Cert Verification. Defaults to None.
+        async_req (Optional[bool], optional): Execute request asynchronously. Defaults to True.
+        show_sql (bool, optional): This will show the sql returned from the server. Defaults to False.
+        show_counts (bool, optional): Currently, the functionality is nonfunctional and pending further investigation to determine if it is needed: This will show or hide the count. Defaults to False.
+        show_term_count (bool, optional): Show the number of occurrences for each value. Defaults to False.
+        verbose (bool, optional): This will hide or show values that are automatic printed when Q runs. Defaults to True.
+        limit (int, optional): the numbers of entries to return per page of data. Defaults to 100.
 
     Returns:
-        Q: _description_
+        Paged_Result
     """
     # cda_client_obj.select_header_content_type(["text/plain"])
     if system:
@@ -117,7 +136,17 @@ def unique_terms(
         q_object._set_system(system)
     else:
         q_object = Q(col_name).unique_terms
-    return q_object
+    return q_object.run(
+        offset=offset,
+        limit=limit,
+        host=host,
+        verify=verify,
+        show_sql=show_sql,
+        show_count=show_counts,
+        show_term_count=show_term_count,
+        verbose=verbose,
+        async_call=async_req,
+    )
 
 
 def columns(
@@ -131,14 +160,12 @@ def columns(
     """
     The Columns method displays all searchable columns in the CDA.
     Args:
-        version (Optional[str], optional): _description_. Defaults to None.
-        host (Optional[str], optional): _description_. Defaults to None.
-        table (Optional[str], optional): _description_. Defaults to None.
-        verify (Optional[bool], optional): _description_. Defaults to None.
-        async_req (Optional[bool], optional): _description_. Defaults to True.
-        show_sql (bool, optional): _description_. Defaults to False.
-        verbose (bool, optional): _description_. Defaults to True.
-        description (bool, optional): _description_. Defaults to True.
+        host (Optional[str], optional): This is where the user can set a host for a different server. Defaults to None.
+        verify (Optional[bool], optional): This will send a request to the cda server without verifying the SSL Cert Verification. Defaults to None.
+        async_req (Optional[bool], optional): Execute request asynchronously. Defaults to True.
+        show_sql (bool, optional): This will show the sql returned from the server. Defaults to False.
+        verbose (bool, optional): This will hide or show values that are automatic printed when Q runs. Defaults to True.
+        description (bool, optional): This parameter will return a description from the server of the columns. Defaults to True.
 
     Returns:
         Optional[ColumnsResult]: _description_
